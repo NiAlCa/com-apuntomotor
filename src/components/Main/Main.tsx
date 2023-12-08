0ad1bcd2ca42823c666c677a3ce5b7f39cdc0bc5
Nicolae <casiean2@gmail.com>
2023-12-08 23:37:23 +0100

    principios del componente main

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,34 @@
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from 'react-bootstrap/Image';
+import logo1 from '../../assets/logo1.png';
+import './Main.scss'; // Asegúrese de que este archivo contenga sus estilos personalizados
+
+export const Main = () => {
+  return (
+    <div className="main-container">
+      <Container>
+        <Row className="justify-content-center align-items-end logo-row">
+          <Col md="auto"className="text-center">
+            <Image src={logo1} className="w-50 center-block" />
+          </Col>
+        </Row>
+        <Row className="justify-content-center align-items-end text-row">
+          <Col md="auto">
+            <h1 className="text-style">TALLER MECÁNICO CHIVA</h1>
+          </Col>
+        </Row>
+        <Row className="justify-content-around align-items-end button-row">
+          <Col xs={12} sm={6} md={4} lg={3}>
+            <Button variant="primary" className="w-100">CONTACTO</Button>
+          </Col>
+          <Col xs={12} sm={6} md={4} lg={3}>
+            <Button variant="primary" className="w-100">SERVICIOS</Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
--- src/pages/Dev/Dev.tsx
+++ src/pages/Dev/Dev.tsx
@@ -1,9 +1,10 @@
+import { Main } from "../../components/Main/Main"
 
 
 export const Dev = () => {
   return (
     <div>
-
+<Main/>
      
     </div>
   )
