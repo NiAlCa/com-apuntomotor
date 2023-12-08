32bdb9389b496451191c03590b285ac500a19311
Nicolae <casiean2@gmail.com>
2023-12-08 22:43:17 +0100

    subiendo rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import './App.css';
 
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage/HomePage';
+import { Dev } from './pages/Dev/Dev';
+
 function App() {
   return (
-    <div className="card">
-      <div className="card-header">
-        <button type="button" className="btn btn-info">Más información</button>
-      </div>
-      <div className="card-body">
-        <h5 className="card-title">Título</h5>
-        <p className="card-text">Texto texto texto texto texto texto texto texto texto texto texto.</p>
-        <img src="path_to_your_image.jpg" className="card-img" alt="Bootstrap" />
-      </div>
-      <div className="card-footer d-flex justify-content-between">
-        <a href="#" className="btn btn-secondary flex-grow-1 mr-2">Volver</a>
-        <a href="#" className="btn btn-primary flex-grow-1 ml-2">Acciones</a>
-      </div>
-    </div>
+    <div className="Principal">
+      
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="dev" element={<Dev />} />
+    </Routes>
+  </div>
   );
 }
 
