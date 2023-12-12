7fd777b9166ab9990f79f4fd14f34b578d710aa2
Nicolae <casiean2@gmail.com>
2023-12-12 14:30:55 +0100

    subiendo componente main y logo 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
-import './index.css'
+import './index.scss'
+import 'animate.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
