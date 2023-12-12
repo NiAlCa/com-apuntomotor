aeaef6a2eed3eecd85a3b5b7cd9efd4f506b8062
Barbara Contardi <barbi.contardi@gmail.com>
2023-12-12 12:03:13 +0100

    refactor: navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,14 +4,17 @@ import '../../styles/variables.scss'
 
 export const Navbar = () => {
   return (
-    <div className="container-navbar">
-      <header className="d-flex flex-wrap justify-content-space-between py-3 mb-4 border-bottom">
+    // <div className="container-navbar">
+      <header className="d-flex flex-wrap justify-content-space-center py-3 border-bottom">
+        <div className='nav-left'>
         <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-          <span className="fs-4">A<img src={punto} alt="punto" width="30" height="30"/>MOTOR</span>
+          <span className="fs-4">A<img src={punto} alt="punto" width="40" height="40"/>MOTOR</span>
         </a>
+        </div>
+        <div className='nav-rigth'>
         <ul className="nav nav-pills">
           <li className="nav-item">
-            <a href="#" className="nav-link active" aria-current="page">INICIO</a>
+            <a href="#" className="nav-link" aria-current="page">INICIO</a>
           </li>
           <li className="nav-item">
             <a href="#" className="nav-link">SERVICIOS</a>
@@ -20,8 +23,9 @@ export const Navbar = () => {
             <a href="#" className="nav-link">CONTACTO</a>
           </li>
         </ul>
+        </div>
       </header>
-    </div>
+    // </div>
   );
 };
 
