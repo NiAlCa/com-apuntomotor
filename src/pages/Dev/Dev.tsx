d318c69666f9882a0b8d36c70fe08dea8713723f
Barbara Contardi <barbi.contardi@gmail.com>
2023-12-15 10:36:53 +0100

    refactor: dev

--- src/pages/Dev/Dev.tsx
+++ src/pages/Dev/Dev.tsx
@@ -1,27 +1,17 @@
-import { Navbar } from "../../components/Navbar/Navbar"
-import { Cards } from "../../components/Cards/Cards"
-import { Carrusell } from "../../components/Carrusell/Carrusell"
-import { Main } from "../../components/Main/Main"
-import Review from "../../components/Review/Review"
+import { Cards } from "../../components/Cards/Cards";
+import { Carrusell } from "../../components/Carrusell/Carrusell";
+import { Main } from "../../components/Main/Main";
+import Review from "../../components/Review/Review";
 
-
-
-
-import './Dev.scss'
-
+import "./Dev.scss";
 
 export const Dev = () => {
   return (
-    <div>
-        <Navbar/>
     <div className="page">
-
-<Main/>
-<Carrusell/>
-<Cards/>
-<Review/>
-     
+      <Main />
+      <Carrusell />
+      <Cards />
+      <Review />
     </div>
-    </div>
-  )
-}
+  );
+};
