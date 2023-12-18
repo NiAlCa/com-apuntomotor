bf22bbb490c1af5dce36ce786ed44de44aac34ee
Barbara Contardi <barbi.contardi@gmail.com>
2023-12-18 10:58:35 +0100

    refactor: navbar y responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,20 @@
-import punto from "../../assets/punto.png";
+import punto from "../../assets/Logo2.png";
 import "./Navbar.scss";
 import "../../styles/variables.scss";
 
 export const Navbar = () => {
   return (
     <header className="d-flex flex-wrap justify-content-space-center border-bottom">
-      <div className="nav-left">
+      <div className="nav-left col-8">
         <a
           href="/"
           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
         >
-          <span className="fs-4">
-            A<img src={punto} alt="punto" width="40" height="40" />
-            MOTOR
-          </span>
+          <img src={punto} alt="punto" width="250" />
         </a>
       </div>
 
-      <ul className="nav nav-pills">
+      <ul className="nav nav-pills col-4">
         <li className="nav-item">
           <a href="#" className="nav-link" aria-current="page">
             INICIO
