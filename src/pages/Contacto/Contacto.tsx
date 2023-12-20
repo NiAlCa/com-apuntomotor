4995fef2cf0815377cfcc9c9f9e5176305f8a495
Barbara Contardi <barbi.contardi@gmail.com>
2023-12-20 14:07:38 +0100

    refactor: detalles

--- src/pages/Contacto/Contacto.tsx
+++ src/pages/Contacto/Contacto.tsx
@@ -1,21 +1,25 @@
-import { ContenedorPage } from "../../components/Contenedor/Contenedor";
 import Form from "../../components/Form/Form";
 import Map from "../../components/Maps/Map";
+import logo from "../../assets/Logo2.png";
+import Menu from "../../components/Menu/Menu";
 
 export const Contacto = () => {
   return (
-    <div>
-      <ContenedorPage bg="bg-light">
-        <br></br>
-        <br></br>
-        <h1 className="text-secondary text-center mx-3">CONTACTANOS</h1>
-        <br></br>
-        <br></br>
+    <div className="row justify-content-center">
+      <div className="col-9 bg-light">
+        <h1 className="text-secondary text-center mx-3 mt-5 mb-5">CONTACTANOS</h1>
         <Map />
         <br></br>
         <br></br>
         <Form />
-      </ContenedorPage>
+      </div>
+
+      <div className="col-3 d-flex flex-column align-items-center menuServicios">
+        <a href="/">
+          <img src={logo} width="250" alt="Logo" />
+        </a>
+        <Menu column={true} />
+      </div>
     </div>
   );
 };
