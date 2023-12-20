a87fec458d465552c3084a2b8a82c0c1b826e1fa
Barbara Contardi <barbi.contardi@gmail.com>
2023-12-20 13:22:28 +0100

    refactor: form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,72 +1,73 @@
-import './Form.scss';
-import { useTranslation } from 'react-i18next';
+import "./Form.scss";
+import { useTranslation } from "react-i18next";
 
 interface FormProps {}
 
 const Form: React.FC<FormProps> = () => {
   const { t } = useTranslation();
   return (
-    <div className='d-flex justify-content-center mt-4'>
-    <div className="col-md-6 templatemo_contactform  w-75 ">
-    <div className="d-flex justify-content-around align-items-center mb-5">
-        <div className="divider" />
-        <h2 className="text-secondary text-center mx-3">
-        {t('servi.formulario')}
-        </h2>
-        <div className="divider" />
+    <div className="d-flex justify-content-center mt-4 mb-4">
+      <div className="col-md-6 templatemo_contactform  w-75 ">
+        <div className="d-flex justify-content-around align-items-center mb-5">
+          <div className="divider" />
+          <h2 className="text-secondary text-center mx-3">
+            {t("servi.formulario")}
+          </h2>
+          <div className="divider" />
+        </div>
+        <form action="enviar.php" method="post">
+          <div className="div-inputs d-flex">
+            <div>
+              <label htmlFor="name">Nombre<span className="required"></span>:</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="name required"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="apellido">Apellido<span className="required"></span>:</label>
+              <input
+                type="text"
+                name="apellido"
+                id="apellido"
+                className="apellido required"
+                required
+              />
+            </div>
+          </div>
+          <label htmlFor="mail">Email<span className="required"></span>:</label>
+          <input
+            type="email"
+            name="mail"
+            id="mail"
+            className="email required"
+            required
+          />
+          <div className="d-flex flex-column phone">
+            <label htmlFor="phone">Teléfono<span className="required"></span>:</label>
+            <input
+              type="number"
+              name="phone"
+              id="subject"
+              className="subject required"
+              min={9}
+              max={12}
+              required
+            />
+          </div>
+          <label htmlFor="message">Mensaje<span className="required"></span>:</label>
+          <textarea name="message" className="message required" id="message" required />
+          <div className="clear"></div>
+          <div className="d-flex justify-content-center">
+            <button type="submit" className="btn btn-primary">
+              {t("servi.formsend")}
+            </button>
+          </div>
+        </form>
       </div>
-      <form action="enviar.php" method="post">
-        <div className='div-inputs d-flex'>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          className="name"
-          placeholder="NOMBRE"
-        />
-         <input
-          type="text"
-          name="apellido"
-          id="apellido"
-          className="apellido"
-          placeholder="APELLIDO"
-        />
-        </div>
-        <div className='div-inputs d-flex'>
-        <input
-          type="text"
-          name="mail"
-          id="mail"
-          className="email"
-          placeholder="CORREO"
-        />
-        <input
-          type="text"
-          name="phone"
-          id="subject"
-          className="subject"
-          placeholder="TELÉFONO"
-        />
-        </div>
-        <textarea
-          name="message"
-          className="message"
-          placeholder="MENSAJE ... "
-          id="message"
-        />
-        <div className="clear"></div>
-        <div className="d-flex justify-content-center">
-
-          <button type="submit" className="btn btn-primary">
-          {t('servi.formsend')}{" "}
-          </button>
-          <button type="submit" className="boton mb-5">
-            ENVIAR{" "}
-
-          </button>
-        </div>
-      </form>
-    </div>
     </div>
   );
 };
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,28 +1,21 @@
 import { Cards } from "../../components/Cards/Cards";
 import { Carrusell } from "../../components/Carrusell/Carrusell";
-
-import  Form  from "../../components/Form/Form";
+import Form from "../../components/Form/Form";
 import { Main } from "../../components/Main/Main";
 import Map from "../../components/Maps/Map";
 import { Navbar } from "../../components/Navbar/Navbar";
 import Review from "../../components/Review/Review";
 import Services from "../../components/Services/Services";
-import image from "../../assets/imagenes/1.jpg"
-import image2 from "../../assets/imagenes/4.jpg"
+import image from "../../assets/imagenes/1.jpg";
+import image2 from "../../assets/imagenes/4.jpg";
 import { ContenedorPage } from "../../components/Contenedor/Contenedor";
 
 export const HomePage = () => {
-
-
-
   return (
     <>
       <Navbar />
       <div className="page  align-items-center flex-column d-flex justify-content-center">
-        <ContenedorPage
-        bi={image }
-        bgFilter={true}
-        >
+        <ContenedorPage bi={image} bgFilter={true}>
           <Main />
         </ContenedorPage>
 
@@ -32,15 +25,19 @@ export const HomePage = () => {
           <Services />
         </ContenedorPage>
 
-      <ContenedorPage bg="bg-light">    <Cards /> </ContenedorPage>
-    <ContenedorPage  bi={image2}
-    > <Review /></ContenedorPage>
-
         <ContenedorPage bg="bg-light">
-        <Map />
-        <Form />
+          {" "}
+          <Cards />{" "}
+        </ContenedorPage>
+        <ContenedorPage bi={image2}>
+          {" "}
+          <Review />
         </ContenedorPage>
 
+        <ContenedorPage bg="bg-light">
+          <Map />
+          <Form />
+        </ContenedorPage>
       </div>
     </>
   );
