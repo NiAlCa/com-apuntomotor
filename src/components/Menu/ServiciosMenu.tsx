be4a4f2a2b4cedefaca314cb32f3776819ae2951
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-12-22 14:58:29 +0100

    refactor

--- src/components/Menu/ServiciosMenu.tsx
+++ src/components/Menu/ServiciosMenu.tsx
@@ -8,16 +8,16 @@ export const ServiciosMenu: React.FC<ServiciosMenuProps> = () => {
   const [dropdown, setDropdown] = useState(false);
   const { t } = useTranslation();
 
-
-
-
-
   const navigateTo = (url: string) => {
     window.location.href = url;
   };
 
   return (
-    <div className={`dropdown mr-sm-5  ${dropdown ? 'show' : ''}`} onMouseOver={() => setDropdown(true)} onMouseLeave={() => setDropdown(false)}>
+    <div
+      className={`dropdown mr-sm-5 ${dropdown ? 'show' : ''}`}
+      onMouseOver={() => setDropdown(true)}
+      onMouseLeave={() => setDropdown(false)}
+    >
       <button
         className="btn btn-transparent dropdown-toggle p-0 border-0"
         type="button"
@@ -27,16 +27,32 @@ export const ServiciosMenu: React.FC<ServiciosMenuProps> = () => {
       >
         {t('services')}
       </button>
-      <ul className={`dropdown-menu ${dropdown ? 'show' : ''}`} aria-labelledby="dropdownMenuButton">
-        <li><a className="dropdown-item" onClick={() => navigateTo('/neumaticos')}>{t('tires')}</a></li>
-        <li><a className="dropdown-item" onClick={() => navigateTo('/aire')}>{t('airConditioning')}</a></li>
-        <li><a className="dropdown-item" onClick={() => navigateTo('/mecanica')}>{t('mechanics')}</a></li>
-        <li><a className="dropdown-item" onClick={() => navigateTo('/luces')}>{t('lightsAnalysis')}</a></li>
-        <li><a className="dropdown-item" onClick={() => navigateTo('/llaves')}>{t('keyDuplication')}</a></li>
-        <li><a className="dropdown-item" onClick={() => navigateTo('/matriculas')}>{t('plates')}</a></li>
-        <li><a className="dropdown-item" onClick={() => navigateTo('/electronica')}>{t('electronics')}</a></li>
-        <li><a className="dropdown-item" onClick={() => navigateTo('/itv')}>{t('preITV')}</a></li>
-      </ul>
+      <div className={`dropdown-menu ${dropdown ? 'show' : ''}`} aria-labelledby="dropdownMenuButton">
+        <a className="dropdown-item" onClick={() => navigateTo('/neumaticos')}>
+          {t('tires')}
+        </a>
+        <a className="dropdown-item" onClick={() => navigateTo('/aire')}>
+          {t('airConditioning')}
+        </a>
+        <a className="dropdown-item" onClick={() => navigateTo('/mecanica')}>
+          {t('mechanics')}
+        </a>
+        <a className="dropdown-item" onClick={() => navigateTo('/luces')}>
+          {t('lightsAnalysis')}
+        </a>
+        <a className="dropdown-item" onClick={() => navigateTo('/llaves')}>
+          {t('keyDuplication')}
+        </a>
+        <a className="dropdown-item" onClick={() => navigateTo('/matriculas')}>
+          {t('plates')}
+        </a>
+        <a className="dropdown-item" onClick={() => navigateTo('/electronica')}>
+          {t('electronics')}
+        </a>
+        <a className="dropdown-item" onClick={() => navigateTo('/itv')}>
+          {t('preITV')}
+        </a>
+      </div>
     </div>
   );
 };
--- src/pages/Servicios/Servicios.tsx
+++ src/pages/Servicios/Servicios.tsx
@@ -1,31 +1,58 @@
-import { Navbar } from "../../components/Navbar/Navbar";
-import { ContenedorPage } from "../../components/Contenedor/Contenedor";
 import { serviciosData } from "../../data/serviciosData.mock";
+import { useTranslation } from 'react-i18next';
+import { I18nextProvider } from 'react-i18next'
 import "./servicios.scss";
+import Menu from '../../components/Menu/Menu';
+import logo from '../../assets/Logo2.webp';
 
 export const Servicios = () => {
+  const { t, i18n } = useTranslation();
   return (
     <>
-      <Navbar />
-      <div className="page  align-items-center flex-column d-flex justify-content-center">
-        <ContenedorPage bg="bg-white">
-          <div className="col align-items-center flex-column d-flex justify-content-center w-100">
-            {serviciosData.map((servicio, index) => (
-              <div key={index} className={`servicio-card ${index % 2 === 1 ? "reverse" : ""}`}>
-                <div className="img-title-card">
-                  <h2>{servicio.title}</h2>
-                  <img className="servicio-img" src={servicio.img} alt={servicio.title} />
-                </div>
-                <div className="text-service">
-                  {servicio.txt.split('\n').map((paragraph, paragraphIndex) => (
-                    <p key={paragraphIndex}>{paragraph}</p>
-                  ))}
-                </div>
+      <I18nextProvider i18n={i18n}>
+        <div className="row  justify-content-center flex-md-row flex-column-reverse">
+          {/* Contenedor de las tarjetas y el botón */}
+          <div className="col-md-8 ">
+            <div className="page align-items-center flex-column d-flex justify-content-center">
+              <div className="col align-items-center flex-column d-flex justify-content-center w-100">
+                {serviciosData.map((servicio, index) => (
+                  <div
+                    key={index}
+                    className={`servicio-card col-xs-12  ${index % 1 === 0 ? "normal" : "reverse"} ${index % 1 === 0 ? "right-animation" : "left-animation"
+                      } animate__animated ${index % 1 === 0 ? "animate__zoomInRight" : "animate__zoomInLeft"}`}
+                  >
+                    <div className="img-title-card">
+                      <h2>{servicio.title}</h2>
+                      <img className="servicio-img" src={servicio.img} alt={servicio.title} />
+                    </div>
+                    <div className="text-service">
+                      {servicio.txt.split('\n').map((paragraph, paragraphIndex) => (
+                        <p key={paragraphIndex}>{paragraph}</p>
+                      ))}
+                    </div>
+                  </div>
+                ))}
               </div>
-            ))}
+                <div className="boton-servicio">
+                  <button className="boton-ser">{t('servi.bottom')}</button>
+                </div>
+            </div>
           </div>
-        </ContenedorPage>
-      </div>
+          
+          {/* Contenedor del menú */}
+          <div className="col-md-4 d-flex  justify-content-center">
+            <div className="col-md-12 col-sm-4 menuServicios d-flex flex-sm-row flex-md-column mt-8  justify-content-md-start justify-content-between px-5 px-sm-5 animate__animated animate__zoomInRight">
+              <a
+                href="/"
+                className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none "
+              >
+                <img src={logo} alt="Logo Apuntomotor" style={{ maxWidth: '200px' }} />
+              </a>
+              <Menu column={true} />
+            </div>
+          </div>
+        </div>
+      </I18nextProvider>
     </>
   );
 };
